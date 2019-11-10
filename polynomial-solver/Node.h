8aa0f3b1999cded8e097ef9270d614bbb2fc74b5
Adam Czarnas <adam.czarnas@gmail.com>
2019-11-10 13:39:54 +0100

    added previous pointer to Node instance

--- polynomial-solver/Node.h
+++ polynomial-solver/Node.h
@@ -5,17 +5,15 @@ template<typename T> class Node
 {
     T _value;
     Node<T>* _next;
+    Node<T>* _prev;
 public:
-    explicit Node(T value, Node<T>* next = nullptr) :
-        _value(value), _next(next){}
+    explicit Node(T value, Node<T>* prev = nullptr, Node<T>* next = nullptr) :
+        _value(value), _prev(prev), _next(next){}
 
-    Node<T>* GetNext(){
-        return _next;
-    }
-
-    void SetNext(Node<T>* next){
-        _next = next;
-    }
+    Node<T>* GetNext(){return _next;}
+    void SetNext(Node<T>* next){_next = next;}
+    Node<T>* GetPrev(){return _prev;}
+    void SetPrev(Node<T>* prev){_prev = prev;}
 };
 
 #endif // NODE_H
