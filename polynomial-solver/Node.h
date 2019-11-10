057d668ab7101b1daccc4d75341557473131b4be
Adam Czarnas <adam.czarnas@gmail.com>
2019-11-10 13:43:36 +0100

    refactor

--- polynomial-solver/Node.h
+++ polynomial-solver/Node.h
@@ -4,16 +4,16 @@
 template<typename T> class Node
 {
     T _value;
-    Node<T>* _next;
-    Node<T>* _prev;
+    Node<T>* p_mNext;
+    Node<T>* p_mPrev;
 public:
     explicit Node(T value, Node<T>* prev = nullptr, Node<T>* next = nullptr) :
-        _value(value), _prev(prev), _next(next){}
+        _value(value), p_mPrev(prev), p_mNext(next){}
 
-    Node<T>* GetNext(){return _next;}
-    void SetNext(Node<T>* next){_next = next;}
-    Node<T>* GetPrev(){return _prev;}
-    void SetPrev(Node<T>* prev){_prev = prev;}
+    Node<T>* GetNext(){return p_mNext;}
+    void SetNext(Node<T>* next){p_mNext = next;}
+    Node<T>* GetPrev(){return p_mPrev;}
+    void SetPrev(Node<T>* prev){p_mPrev = prev;}
 };
 
 #endif // NODE_H
