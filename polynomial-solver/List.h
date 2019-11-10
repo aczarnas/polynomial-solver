8f60089dc748244a6dbffb6f1bd7a6f8183aa1b7
Adam Czarnas <adam.czarnas@gmail.com>
2019-11-10 17:45:33 +0100

    separated clear method from destructor

--- polynomial-solver/List.h
+++ polynomial-solver/List.h
@@ -19,21 +19,7 @@ public:
     }
 
     ~List(){
-        if(mSize == 0){
-            return;
-        }
-        if(mSize == 1){
-            delete p_mHead;
-            return;
-        }
-        Node<T>* handler = p_mHead;
-        while(handler != nullptr){
-            Node<T>* next = handler->getNext();
-            delete handler;
-            --mSize;
-            handler = next;
-        }
-
+        clear();
     }
 
     Node<T>* getHead() {return p_mHead;}
@@ -49,8 +35,18 @@ public:
             el->setPrev(p_mTail);
             p_mTail = el;
         }
-
         ++mSize;
+    }
+
+    void clear(){
+        Node<T>* handler = p_mHead;
+        while(handler != nullptr){
+            Node<T>* next = handler->getNext();
+            delete handler;
+            --mSize;
+            handler = next;
+        }
+        p_mHead = p_mTail = nullptr;
     }
 };
 
